70a6d1855ee17fc354f8f96f7d44a64988a39ac5
moravechynek <moravechynek@seznam.cz>
2021-03-15 17:05:33 +0100

    První zpráva

--- index.js
+++ index.js
@@ -15,4 +15,8 @@ let io = socket(server);
 
 io.on('connection', function(socket){
     console.log('made socket connection', socket.id);
+    
+    socket.on('chat', function(data){
+        io.sockets.emit('chat', data);
+    });
 });
--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,23 @@
 // Make connection
 let socket = io.connect("http://localhost:8080");
+
+//  Query DOM
+let message = document.getElementById('message');
+let handle = document.getElementById('handle');
+let btn = document.getElementById('send');
+let output = document.getElementById('output');
+
+// Emit events
+
+btn.addEventListener('click', function(){
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
+    });
+});
+
+//Listen for events
+socket.on('chat', function(data){
+    output.innerHTML += `<p><strong>${data.handle}:</strong>${data.message}</p>`;
+    //output.innerHTML += '<p><strong>' + data.handle + '</strong>' + data.message + '</p>';
+});
